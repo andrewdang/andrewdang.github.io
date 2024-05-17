f299354ae02d90f08220bb942c906b05623b5698
Andrew Dang <andrew.dang17@gmail.com>
2024-05-16 19:34:15 -0700

    test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
   url: "https://andrewdang.github.io",
   base: "/andrewdang.github.io",
